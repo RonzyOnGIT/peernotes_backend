2a6dc7576161398add361a4a54f8df5e8015b85c
= <ninjakai25@gmail.com>
2025-10-05 09:15:23 -0500

    add cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors";
 import { scrapeCourses } from '../utils/scraper';
 import type { Course } from '../utils/scraper';
 
@@ -8,6 +9,13 @@ interface FileMetadata {
 
 const app = express();
 const PORT = 3001;
+
+// allow request from frontend
+app.use(cors({
+  origin: "http://localhost:3000", // your frontend URL
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "ngrok-skip-browser-warning", "x-api-key"] // include any custom headers
+}));
 
 // returns all courses
 app.get('/courses', async (_req, res) => {
@@ -23,6 +31,20 @@ app.get('/courses', async (_req, res) => {
 
 
 
+
+// user will fill out form
+/*
+
+    So basically for the post request it will make 
+    File name (string)
+    Description (string)
+    export type Course = {
+        department: string | null; // CSE
+        code: string | null; // 1325
+        course_name: string | null; // Object oriented 
+    }
+
+*/
 // set up endpoint for post request
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
